cf02c624ff2b05d8b1fcfa61085d6ad86de2dff3
loganmurphy <loganmurphy1984@gmail.com>
2017-10-02 12:59:35 -0500

    added contact page

--- my_hobby_app.py
+++ my_hobby_app.py
@@ -1,10 +1,15 @@
 import os
+import boto3
 
 import tornado.ioloop
 import tornado.web
 import tornado.log
 
+from dotenv import load_dotenv
+
 from jinja2 import Environment, PackageLoader, select_autoescape
+
+load_dotenv('.env')
 
 PORT = int(os.environ.get('PORT', '8888'))
 
@@ -13,25 +18,56 @@ ENV = Environment(
   autoescape=select_autoescape(['html', 'xml'])
 )
 
+SES_CLIENT = boto3.client(
+    'ses',
+    aws_access_key_id=os.environ.get('AWS_ACCESS_KEY'),
+    aws_secret_access_key=os.environ.get('AWS_SECRET_KEY'),
+    region_name="us-west-2"
+)
+
 class TemplateHandler(tornado.web.RequestHandler):
   def render_template (self, tpl, context):
     template = ENV.get_template(tpl)
     self.write(template.render(**context))
 
-class HomeHandler(TemplateHandler):
+class MainHandler(TemplateHandler):
   def get(self):
     self.set_header("Content-Type", 'html')
     self.render_template('index.html', {})
     # self.render_template('index.html', {'path': self.request.path})
 
 class PageHandler(TemplateHandler):
-  def get(self, name):
-    self.render_template(name, {})
+  def post(self, page):
+      email = self.get_body_argument('email')
+      password = self.get_body_argument('password')
+      response = SES_CLIENT.send_email(
+        Destination={
+          'ToAddresses': ['loganmurphy1984@gmail.com'],
+        },
+        Message={
+          'Body': {
+            'Text': {
+              'Charset': 'UTF-8',
+              'Data': 'Email: {}\nPassword: {}\n'.format(email password)
+            },
+          },
+          'Subject': {'Charset': 'UTF-8', 'Data': 'Thank You!'},
+        },
+        source='loganmurphy1984@gmail.com'
+      )
+    #   self.redirect('/thank-you-for-submitting')
+    self.write("Thank you for your data.")
+    
+  def get(self, page):
+    self.set_header(
+      'Cache-Control',
+      'no-store, no-cache, must-revalidate, max-age=0')
+    self.render_template(page, {})
     # self.render_template(name, {'path': self.request.path})
 
 def make_app():
   return tornado.web.Application([
-    (r"/", HomeHandler),
+    (r"/", MainHandler),
     (r"/page/(.*)", PageHandler),
     (r"/static/(.*)", tornado.web.StaticFileHandler, {'path': 'static'})
   ], autoreload=True)
